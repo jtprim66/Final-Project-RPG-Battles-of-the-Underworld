9162d681189da888f6cc3f63535e8d9b64239e98
jtprim66 <jtprim66@students.davisonschools.org>
2016-03-31 10:19:30 -0400

    Battle Selection
    Added options for selecting enemies to battle and adjustments to enemy stats based on the enemy selected.

--- RPG Source.cpp
+++ RPG Source.cpp
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string>
 #include "Characters.h"
+#include "Shop.h"
 
 using namespace std;
 
 int main(){
 	float PlayerHealth = 0;
+	float PlayerMaxHealth = 0;
 	float PlayerAttack = 0;
 	float PlayerDefense = 0;
 	float PlayerDefensePercent = 0;
@@ -44,9 +46,11 @@ int main(){
 
 	int menu = 0;
 	int ShopMenu = 0;
+	int EnemySelection = 0;
 
 	PlayableCharacters pchar;
 	Enemies nmy;
+	Shop shop;
 
 	cout << "Welcome to Battles of The Underworld" << endl;
 	cout << "Please select a character class: " << endl;
@@ -90,6 +94,7 @@ int main(){
 
 	cout << endl;
 
+	PlayerMaxHealth = PlayerHealth;
 	PlayerDefensePercent = PlayerDefense;
 	PlayerDefensePercent /= 10;
 
@@ -196,17 +201,18 @@ int main(){
 		cout << endl;
 	}
 
-	cout << "Welcome to the menu! Please make a selection (1-4)" << endl;
-	cout << endl;
-
-	cout << "1. Heal" << endl;
-	cout << "2. Shop" << endl;
-	cout << "3. Battle" << endl;
-	cout << "4. Quit" << endl;
-
-	cout << "Your selection: " << endl;
+	
 
 	do{
+		cout << "Welcome to the menu! Please make a selection (1-4)" << endl;
+		cout << endl;
+
+		cout << "1. Heal" << endl;
+		cout << "2. Shop" << endl;
+		cout << "3. Battle" << endl;
+		cout << "4. Quit" << endl;
+
+		cout << "Your selection: " << endl;
 
 		cin >> menu;
 		switch (menu){
@@ -214,47 +220,7 @@ int main(){
 		case 1:
 			PlayerHealth = PlayerMaxHealth;
 			cout << "Your health has been fully restored!" << endl;
-			break;
-
-		case 2:
-			cout << "Welcome to the shop! What would you like to purchase?" << endl;
 			cout << endl;
-
-			cout << "1. Potion   $" << shop.getPotionCost() << endl;
-			cout << "2. Basic Armor   $" << shop.getBasicArmorCost() << endl;
-			cout << "3. Sturdy Armor   $" << shop.getSturdyArmorCost() << endl;
-			cout << "4. Strong Armor   $" << shop.getStrongArmorCost() << endl;
-			cout << "5. Olympian Armor   $" << shop.getOlympianArmorCost() << endl;
-
-			if (PlayerClass == A){
-				cout << "6. Longbow   $" << shop.getLongbowCost() << endl;
-				cout << "7. Enchanted Bow   $" << shop.getEnchantedBowCost() << endl;
-				cout << "8. Bow of Apollo   $" << shop.getBowOfApolloCost() << endl;
-			}
-			if (PlayerClass == B){
-				cout << "6. Thick Club   $" << shop.getThickClubCost() << endl;
-				cout << "7. Enchanted Mace   $" << shop.getEnchantedMaceCost() << endl;
-				cout << "8. Hammer of Hephaestus   $" << shop.getHammerOfHephaestusCost() << endl;
-			}
-			if (PlayerClass == S){
-				cout << "6. Bronze Dagger   $" << shop.getBronzeDaggerCost() << endl;
-				cout << "7. Enchanted Gladius   $" << shop.getEnchantedGladiusCost() << endl;
-				cout << "8. Sword of Ares   $" << shop.getSwordOfAresCost() << endl;
-			}
-
-			cout << "9. Exit" << endl;
-			cout << endl;
-			cout << "Make your selection (1-9)" << endl;
-
-
-	do{
-
-		cin >> menu;
-		switch (menu){
-
-		case 1:
-			PlayerHealth = PlayerMaxHealth;
-			cout << "Your health has been fully restored!" << endl;
 			break;
 
 		case 2:
@@ -476,16 +442,224 @@ int main(){
 
 			} while (ShopMenu != 9);
 
-			cout << "Welcome to the menu! Please make a selection (1-4)" << endl;
-			cout << endl;
+			case 3:
+				cout << "Select an enemy to battle:" << endl;
+				cout << endl;
 
-			cout << "1. Heal" << endl;
-			cout << "2. Shop" << endl;
-			cout << "3. Battle" << endl;
-			cout << "4. Quit" << endl;
+				cout << "1. " << ws << endl;
+				cout << "   Health: " << nmy.getwsHealth() << endl;
+				cout << "   Attack: " << nmy.getwsAttack() << endl;
+				cout << endl;
 
-			cout << "Your selection: " << endl;
-			//Insert battle option here next
+				cout << "2. " << vs << endl;
+				cout << "   Health: " << nmy.getvsHealth() << endl;
+				cout << "   Attack: " << nmy.getvsAttack() << endl;
+				cout << endl;
+
+				cout << "3. " << h << endl;
+				cout << "   Health: " << nmy.gethHealth() << endl;
+				cout << "   Attack: " << nmy.gethAttack() << endl;
+				cout << endl;
+
+				cout << "4. " << efe << endl;
+				cout << "   Health: " << nmy.getefeHealth() << endl;
+				cout << "   Attack: " << nmy.getefeAttack() << endl;
+				cout << endl;
+
+				cout << "5. " << e << endl;
+				cout << "   Health: " << nmy.geteHealth() << endl;
+				cout << "   Attack: " << nmy.geteAttack() << endl;
+				cout << endl;
+
+				cout << "6. " << c << endl;
+				cout << "   Health: " << nmy.getcHealth() << endl;
+				cout << "   Attack: " << nmy.getcAttack() << endl;
+				cout << endl;
+
+				cout << "7. " << hgs << endl;
+				cout << "   Health: " << nmy.gethgsHealth() << endl;
+				cout << "   Attack: " << nmy.gethgsAttack() << endl;
+				cout << endl;
+
+				cout << "8. " << tgd << endl;
+				cout << "   Health: " << nmy.gettgdHealth() << endl;
+				cout << "   Attack: " << nmy.gettgdAttack() << endl;
+				cout << endl;
+
+				cout << "9. " << hgu << endl;
+				cout << "   Health: " << nmy.gethguHealth() << endl;
+				cout << "   Attack: " << nmy.gethguAttack() << endl;
+				cout << endl;
+
+				cout << "10. " << tsbp << endl;
+				cout << "   Health: " << nmy.gettsbpHealth() << endl;
+				cout << "   Attack: " << nmy.gettsbpAttack() << endl;
+				cout << endl;
+
+				cout << endl;
+				cout << "Enter the number of your selection:" << endl;
+
+				cin >> EnemySelection;
+
+				switch (EnemySelection){
+
+				case 1:
+					EnemyHealth = nmy.getwsHealth();
+					EnemyAttack = nmy.getwsAttack();
+					EnemyGold = nmy.getwsGold();
+					EnemyName = ws;
+					break;
+
+				case 2:
+					EnemyHealth = nmy.getvsHealth();
+					EnemyAttack = nmy.getvsAttack();
+					EnemyGold = nmy.getvsGold();
+					EnemyName = vs;
+					break;
+
+				case 3:
+					EnemyHealth = nmy.gethHealth();
+					EnemyAttack = nmy.gethAttack();
+					EnemyGold = nmy.gethGold();
+					EnemyName = h;
+					break;
+
+				case 4:
+					EnemyHealth = nmy.getefeHealth();
+					EnemyAttack = nmy.getefeAttack();
+					EnemyGold = nmy.getefeGold();
+					EnemyName = efe;
+					break;
+
+				case 5:
+					EnemyHealth = nmy.geteHealth();
+					EnemyAttack = nmy.geteAttack();
+					EnemyGold = nmy.geteGold();
+					EnemyName = e;
+					break;
+
+				case 6:
+					EnemyHealth = nmy.getcHealth();
+					EnemyAttack = nmy.getcAttack();
+					EnemyGold = nmy.getcGold();
+					EnemyName = c;
+					break;
+
+				case 7:
+					EnemyHealth = nmy.gethgsHealth();
+					EnemyAttack = nmy.gethgsAttack();
+					EnemyGold = nmy.gethgsGold();
+					EnemyName = hgs;
+					break;
+
+				case 8:
+					EnemyHealth = nmy.gettgdHealth();
+					EnemyAttack = nmy.gettgdAttack();
+					EnemyGold = nmy.gettgdGold();
+					EnemyName = tgd;
+					break;
+
+				case 9:
+					EnemyHealth = nmy.gethguHealth();
+					EnemyAttack = nmy.gethguAttack();
+					EnemyGold = nmy.gethguGold();
+					EnemyName = hgu;
+					break;
+
+				case 10:
+					EnemyHealth = nmy.gettsbpHealth();
+					EnemyAttack = nmy.gettsbpAttack();
+					EnemyGold = nmy.gettsbpGold();
+					EnemyName = tsbp;
+					break;
+				}
+
+				EnemyAttackPercent = EnemyAttack;
+				EnemyAttackPercent *= PlayerDefensePercent;
+				EnemyAttack -= EnemyAttackPercent;
+				cout << endl;
+
+				do{
+					cout << EnemyName << ": " << EnemyHealth << " health" << endl;
+					cout << PlayerName << ": " << PlayerHealth << " health" << endl;
+					cout << endl;
+
+					cout << "1. Attack" << endl;
+					cout << "2. Use potion (" << PlayerPotion << " potions in inventory)" << endl;
+					cout << "3. Retreat (This wil count as a loss but no money will be lost)" << endl;
+					cout << endl;
+					cout << "Enter the number of your option: " << endl;
+
+					cin >> BattleMenu;
+					cout << endl;
+					switch (BattleMenu){
+
+					case 1:
+						EnemyHealth -= PlayerAttack;
+						cout << PlayerName << " attacked!" << endl;
+						cout << EnemyName << "'s health is now " << EnemyHealth << endl;
+						cout << endl;
+						break;
+
+					case 2:
+						if (PlayerPotion > 0){
+							PlayerHealth += 25;
+							cout << PlayerName << " used a potion!" << endl;
+							cout << endl;
+							break;
+						}
+						else{
+							cout << "You have no potions remaining!" << endl;
+							cout << endl;
+							continue;
+						}
+
+					case 3:
+						cout << PlayerName << " has fled!" << endl;
+						cout << endl;
+						RoundLoss = 1;
+						Losses += 1;
+						break;
+
+					default:
+						cout << "That is not an option" << endl;
+						continue;
+					}
+
+					if (EnemyHealth > 0 && RoundLoss != 1){
+						PlayerHealth -= EnemyAttack;
+						cout << EnemyName << " attacked!" << endl;
+						cout << PlayerName << "'s health is now " << PlayerHealth << endl;
+						cout << endl;
+					}
+
+				} while (RoundLoss != 1 && PlayerHealth > 0 && EnemyHealth > 0);
+
+				if (PlayerHealth > 0 && RoundLoss != 1){
+					cout << "Victory! You have defeated " << EnemyName << "!" << endl;
+					cout << "Gold earned: " << EnemyGold << endl;
+					PlayerGold += EnemyGold;
+					cout << endl;
+				}
+
+				if (PlayerHealth <= 0){
+					PlayerGold = LossGold;
+					LossGold /= DivideGold;
+					cout << "Deafeat! Your enemy has bested you. Return to the menu and heal." << endl;
+					cout << "Gold lost: " << LossGold;
+					PlayerGold -= LossGold;
+					cout << endl;
+				}
+
+				if (RoundLoss != 0){
+					cout << "Defeat! You were forced to retreat. Return to the menu and retry." << endl;
+					cout << endl;
+				}
+				break;
+
+				case 4:
+					cout << "Thank you for playing!" << endl;
+					break;
 		}
 
 	} while (menu != 4);
