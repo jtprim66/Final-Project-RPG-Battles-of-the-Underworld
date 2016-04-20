2bd178f754c961fb0689461fee8e4cef777c9bf8
jtprim66 <jtprim66@students.davisonschools.org>
2016-04-20 10:58:44 -0400

    Hades Monologue and Health Bonus
    Added story line monologue for after defeating Hades which includes a 50% health boost to the player's character.

--- RPG Source.cpp
+++ RPG Source.cpp
@@ -681,6 +681,21 @@ int main(){
 						system("pause");
 						return 1;
 					}
+
+						if (EnemyName == hgu){
+							cout << "Upon defeating Hades, " << PlayerName << endl;
+							cout << " found a flask filled with the nectar" << endl;
+							cout << " of the gods! " << PlayerName << " drank of" << endl;
+							cout << " the nectar and felt a great feeling of" << endl;
+							cout << " strength as the food of the immortal gods " << endl;
+							cout << " brought the hero one step closer to" << endl;
+							cout << " immortality." << endl;
+							cout << endl;
+							PlayerMaxHealth = 150;
+							PlayerHealth = PlayerMaxHealth;
+							cout << "Your health is now 150!" << endl;
+							cout << endl;
+					}
 				}
 
 				if (PlayerHealth <= 0){
